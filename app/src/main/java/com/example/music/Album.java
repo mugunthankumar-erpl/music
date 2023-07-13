68312f08200124cda6405f25ec9116f6d76ae770
Mugunthan Kumar <mugunthankumar.selvakumar@erplogic.com>
2023-07-13 13:33:31 +0530

    ss

--- app/src/main/java/com/example/music/Album.java
+++ app/src/main/java/com/example/music/Album.java
@@ -19,7 +19,7 @@ public class Album extends Fragment {
     View view;
     public ListView albumListView;
     public List<String> albumList;
-    TextView album; // mugunthan
+    TextView album; // mugunthans
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
